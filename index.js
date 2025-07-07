f3cb2175c00d5efe4b1db1652ec48e68614bd08a
sven.bonnaud <sven.bonnaud@edu.ecole-89.com>
2025-07-07 11:57:18 +0200

    test + - affichage

---
+++ index.js
@@ -0,0 +1,9 @@
+<script>
+function toggleDetails(button) {
+  const details = button.nextElementSibling;
+  const isVisible = details.style.display === 'block';
+
+  details.style.display = isVisible ? 'none' : 'block';
+  button.innerHTML = isVisible ? '➕ Réécriture de la commande cat en C' : '➖ Réécriture de la commande cat en C';
+}
+</script>
